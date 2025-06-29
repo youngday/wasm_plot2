949b51ff6ebd9ab2d98d8e23ba85fec01cf3d9fa
youngday <youngday@aliyun.com>
2025-06-29 14:24:22 +0800

    update and disable other examples

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,6 @@
 use crate::pages::{
     demo::Demo,
-    examples::{view_example, Example, Examples},
+    // examples::{view_example, Example, Examples},
 };
 use leptos::prelude::*;
 use leptos_meta::provide_meta_context;
@@ -25,27 +25,27 @@ pub fn App() -> impl IntoView {
             <main>
                 <Routes fallback=NotFound>
                     <Route path=StaticSegment("/") view=Demo />
-                    <Route path=StaticSegment("/examples.html") view=Examples />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::Line.id()), StaticSegment(".html")) view=|| view_example(Example::Line) />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::StackedLine.id()), StaticSegment(".html")) view=|| view_example(Example::StackedLine) />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::Bar.id()), StaticSegment(".html")) view=|| view_example(Example::Bar) />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::Legend.id()), StaticSegment(".html")) view=|| view_example(Example::Legend) />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::TickLabels.id()), StaticSegment(".html")) view=|| view_example(Example::TickLabels) />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::RotatedLabel.id()), StaticSegment(".html")) view=|| view_example(Example::RotatedLabel) />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::EdgeLayout.id()), StaticSegment(".html")) view=|| view_example(Example::EdgeLayout) />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::AxisMarker.id()), StaticSegment(".html")) view=|| view_example(Example::AxisMarker) />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::GridLine.id()), StaticSegment(".html")) view=|| view_example(Example::GridLine) />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::GuideLine.id()), StaticSegment(".html")) view=|| view_example(Example::GuideLine) />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::InsetLegend.id()), StaticSegment(".html")) view=|| view_example(Example::InsetLegend) />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::InnerLayout.id()), StaticSegment(".html")) view=|| view_example(Example::InnerLayout) />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::MixedInterpolation.id()), StaticSegment(".html")) view=|| view_example(Example::MixedInterpolation) />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::Stepped.id()), StaticSegment(".html")) view=|| view_example(Example::Stepped) />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::Tooltip.id()), StaticSegment(".html")) view=|| view_example(Example::Tooltip) />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::Colours.id()), StaticSegment(".html")) view=|| view_example(Example::Colours) />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::Markers.id()), StaticSegment(".html")) view=|| view_example(Example::Markers) />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::Markers2.id()), StaticSegment(".html")) view=|| view_example(Example::Markers2) />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::LineGradient.id()), StaticSegment(".html")) view=|| view_example(Example::LineGradient) />
-                    <Route path=(StaticSegment("/examples/"), StaticSegment(Example::Css.id()), StaticSegment(".html")) view=|| view_example(Example::Css) />
+                    // <Route path=StaticSegment("/examples.html") view=Examples />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::Line.id()), StaticSegment(".html")) view=|| view_example(Example::Line) />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::StackedLine.id()), StaticSegment(".html")) view=|| view_example(Example::StackedLine) />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::Bar.id()), StaticSegment(".html")) view=|| view_example(Example::Bar) />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::Legend.id()), StaticSegment(".html")) view=|| view_example(Example::Legend) />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::TickLabels.id()), StaticSegment(".html")) view=|| view_example(Example::TickLabels) />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::RotatedLabel.id()), StaticSegment(".html")) view=|| view_example(Example::RotatedLabel) />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::EdgeLayout.id()), StaticSegment(".html")) view=|| view_example(Example::EdgeLayout) />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::AxisMarker.id()), StaticSegment(".html")) view=|| view_example(Example::AxisMarker) />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::GridLine.id()), StaticSegment(".html")) view=|| view_example(Example::GridLine) />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::GuideLine.id()), StaticSegment(".html")) view=|| view_example(Example::GuideLine) />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::InsetLegend.id()), StaticSegment(".html")) view=|| view_example(Example::InsetLegend) />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::InnerLayout.id()), StaticSegment(".html")) view=|| view_example(Example::InnerLayout) />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::MixedInterpolation.id()), StaticSegment(".html")) view=|| view_example(Example::MixedInterpolation) />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::Stepped.id()), StaticSegment(".html")) view=|| view_example(Example::Stepped) />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::Tooltip.id()), StaticSegment(".html")) view=|| view_example(Example::Tooltip) />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::Colours.id()), StaticSegment(".html")) view=|| view_example(Example::Colours) />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::Markers.id()), StaticSegment(".html")) view=|| view_example(Example::Markers) />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::Markers2.id()), StaticSegment(".html")) view=|| view_example(Example::Markers2) />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::LineGradient.id()), StaticSegment(".html")) view=|| view_example(Example::LineGradient) />
+                    // <Route path=(StaticSegment("/examples/"), StaticSegment(Example::Css.id()), StaticSegment(".html")) view=|| view_example(Example::Css) />
                 </Routes>
             </main>
         </Router>
